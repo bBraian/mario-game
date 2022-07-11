43434d2dd75a709bba05ac9f647ecd222c6976ea
Braian <braianvoficial@gmail.com>
2022-07-11 09:12:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './assets/css/styles.css';
+import Pipe from './assets/images/pipe.png';
+import Mario from './assets/images/mario.gif';
 
 function App() {
+  const [ marioJump, setMarioJump ] = useState(false); 
+  document.addEventListener('keydown', handleMarioJump);
+
+  function handleMarioJump() {
+    if(!marioJump) {
+      setMarioJump(true);
+      setTimeout(() => {
+        setMarioJump(false);
+      }, 500)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='game-board'>
+        <img className={marioJump?'mario jump':'mario'} src={Mario} alt="mario"></img>
+        <img className='pipe' src={Pipe} alt="pipe"></img>
+      </div>
+      <button onClick={handleMarioJump}>
+        Pular
+      </button>
+    </>
+    
   );
 }
 
